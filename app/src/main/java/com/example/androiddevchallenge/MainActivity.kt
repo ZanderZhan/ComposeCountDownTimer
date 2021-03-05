2d124f24b2709985ad7a0cea48adb3825c4e2f3b
Zander <guyszdc@163.com>
2021-03-05 17:49:12 +0800

    countdown timer

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,13 +16,34 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.text.format.DateUtils
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -40,22 +61,105 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        val context = LocalContext.current
+        val durationState = remember { mutableStateOf(0) }
+        val targetState = remember { mutableStateOf(0) }
+        val startState = remember { mutableStateOf(false) }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            CountDownView(start = startState.value, target = targetState.value, duration = durationState.value) {
+//                Log.i("zander", "finish: $it, start: ${startState.value}")
+                if (startState.value && it != 0) {
+                    durationState.value = it * 1000
+                    targetState.value = 0
+                } else if (startState.value && it == 0) {
+                    startState.value = false
+                    Toast.makeText(context, "Times up !!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            InputView(startState.value) {
+                startState.value = !startState.value
+                targetState.value = if (startState.value) it else 0
+                durationState.value = 0
+            }
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
+fun InputView(start: Boolean, onClick: (Int) -> Unit) {
+    val hourState = remember { mutableStateOf("") }
+    val minuteState = remember { mutableStateOf("") }
+    val secondState = remember { mutableStateOf("") }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            value = hourState.value,
+            onValueChange = {
+                hourState.value = it
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = {
+                Text("H")
+            }
+        )
+        OutlinedTextField(
+            value = minuteState.value,
+            onValueChange = {
+                minuteState.value = it
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = {
+                Text("M")
+            }
+        )
+        OutlinedTextField(
+            value = secondState.value,
+            onValueChange = {
+                secondState.value = it
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = {
+                Text("S")
+            }
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = {
+                var seconds = 0
+                if (hourState.value.isNotEmpty()) {
+                    seconds += hourState.value.toInt() * 3600
+                }
+                if (minuteState.value.isNotEmpty()) {
+                    seconds += minuteState.value.toInt() * 60
+                }
+                if (secondState.value.isNotEmpty()) {
+                    seconds += secondState.value.toInt()
+                }
+                onClick.invoke(seconds)
+            }
+        ) {
+            Text(if (start) "Stop !!" else "Start !!")
+        }
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
-    }
+fun CountDownView(start: Boolean, target: Int, duration: Int, finish: (Int) -> Unit) {
+//    Log.i("zander", "start: $start, target: ${target}")
+    val current = animateIntAsState(
+        targetValue = target,
+        animationSpec = tween(
+            durationMillis = duration,
+            easing = LinearEasing
+        ),
+        finishedListener = finish
+    )
+    Text(
+        text = DateUtils.formatElapsedTime(current.value.toLong()),
+        color = Color.Black,
+        fontSize = 50.sp,
+        modifier = Modifier.padding(0.dp, 30.dp)
+    )
 }
